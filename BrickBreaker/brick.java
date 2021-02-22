4c62a772e5d8afb647f8a80a4977c8f084fcf317
Ekankijain <ekankijain123@gmail.com>
2021-02-22 11:41:22 +0530

    Projects

---
+++ BrickBreaker/brick.java
@@ -0,0 +1,19 @@
+package project;
+
+import javax.swing.JFrame;
+
+
+public class BrickBreaker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JFrame obj = new JFrame();
+		obj.setBounds(10,10,800,700);
+		obj.setTitle("BRICK BREAKKKK");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperations(JFrame.EXIT);
+	}
+
+}
